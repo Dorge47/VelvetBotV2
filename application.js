3e09d907a1e5b3999204fae737205f31f130c7d5
Dorge47 <Chris_jandrade@yahoo.com>
2019-03-26 14:55:42 -0700

    Added getMessages()

--- application.js
+++ application.js
@@ -1,1 +1,19 @@
-var content = JSON.parse(document.getElementsByTagName('pre')[0].innerText)
+var token = '625045601:AAF4Kr-a7yJRGVUU9ssi0MI79NZDeN-RUws'
+
+function getMessages() {
+    var url = 'https://api.telegram.org/bot' + token + '/getUpdates'
+    fetch(url, {
+        headers: {
+            'Cache-Control': 'no-store' //avoid getting messages from cache
+        }
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        messagesGotten = myJson;
+    });
+    setTimeout(function() {
+        messages = messagesGotten.result
+    }, 1000)
+}
