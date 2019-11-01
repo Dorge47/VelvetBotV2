382f5fd06235a2eff434e122b3307afa896a4e7f
Chris Andrade <Dorge47@users.noreply.github.com>
2019-10-31 22:00:02 -0700

    Fix JSON parsing

--- main.js
+++ main.js
@@ -437,7 +437,7 @@ function parseCaptionedIdList(fileName) {
 
 function parseComplexList(fileName) {
     let complexList = [];
-    let file = JSON.parse(fs.readFileSync("./" + exports.directory + "/" + fileName));
+    let file = JSON.parse(JSON.stringify(fs.readFileSync("./" + exports.directory + "/" + fileName)));
     for (i = 0; i < file.length; i++) {
         if (file[i].fileType == 'photo') {
             if (file[i].caption == null) {
