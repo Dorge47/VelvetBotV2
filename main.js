3723590ced492a23b07cc74d9d9f904b028cd7a0
Chris Andrade <Dorge47@users.noreply.github.com>
2019-11-01 15:53:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -92,8 +92,8 @@ exports.callback = function(message) {
     //Check to see if any of the messages match a command
     let messageProcessed = false;
     console.log(message.text.toLowerCase().substring(2));
+    console.log(JSON.stringify(commands));
     for (let i = 0; i < Object.keys(commands).length; i++) {
-    	console.log(Object.keys(commands)[i]);
         if (message.text.toLowerCase().substring(2).includes(Object.keys(commands)[i])) {
             processCommand(commands[Object.keys(commands)[i]], message);
             messageProcessed = true;
