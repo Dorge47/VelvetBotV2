4ba18cdff054cac185e8f687ff78f6d3afce2083
Chris Andrade <Dorge47@users.noreply.github.com>
2019-11-01 16:12:32 -0700

    Update debug logs

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ exports.callback = function(message) {
     //Check to see if any of the messages match a command
     let messageProcessed = false;
     console.log(message.text.toLowerCase().substring(2));
-    console.log(JSON.stringify(commands));
+    console.log(fs.readFileSync("./" + exports.directory + '/redditcommands.json'))
     for (let i = 0; i < Object.keys(commands).length; i++) {
         if (message.text.toLowerCase().substring(2).includes(Object.keys(commands)[i])) {
             processCommand(commands[Object.keys(commands)[i]], message);
