6664d6ad26eb1cc2791cbb7e3551f20139927441
Nathaniel Barragan <nathanielbarragan@gmail.com>
2020-02-27 12:25:54 -0800

    Use trim to remove whitespace

--- main.js
+++ main.js
@@ -259,7 +259,7 @@ function processCommand(command, message) {
                             total += roll;
                             response += roll + ' ';
                         }
-                        response = response.slice(0, response.length);  // remove ending whitespace
+                        response = response.trim();
                         response += "\ntotal: " + total;
                     }
                     bot.sendReply(message.chat.id, response, message.message_id);
